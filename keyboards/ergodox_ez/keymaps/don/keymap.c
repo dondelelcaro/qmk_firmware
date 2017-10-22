35abd0dbe8675e14cf7b1c2aa019fd86263eea65
Don Armstrong <don@donarmstrong.com>
2017-10-22 16:42:04 -0700

    move my keymap to new location and include QMK_KEYBOARD_H

--- keyboards/ergodox_ez/keymaps/don/keymap.c
+++ keyboards/ergodox_ez/keymaps/don/keymap.c
@@ -16,7 +16,7 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-#include "ergodox.h"
+#include QMK_KEYBOARD_H
 #include "debug.h"
 #include "action_layer.h"
 #include "version.h"
